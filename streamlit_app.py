c87031600ef831e6284ae4a2bcfa502615f78ea0
rv2709 <144166160+rv2709@users.noreply.github.com>
2023-09-07 11:00:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,6 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The food load list contains")
-streamlit.text(my_data_row)
+streamlit.header("The food load list contains:")
+streamlit.dataframe(my_data_row)
 
