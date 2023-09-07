8da35f68b5d5ab3747c789dea871b2753bcb6f0f
rv2709 <144166160+rv2709@users.noreply.github.com>
2023-09-07 11:35:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-#import pandas
-#import requests
-#import snowflake.connector
+import pandas
+import requests
+import snowflake.connector
 from urllib.error import URLError
 
 # Update streamlit_app.py
