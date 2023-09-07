42536618af161660c52d56560490c0d019d295f8
rv2709 <144166160+rv2709@users.noreply.github.com>
2023-09-07 08:03:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 # Update streamlit_app.py
 
 streamlit.title('My Parents New Healthy Diner')
@@ -22,3 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.text(fruityvice_response)
